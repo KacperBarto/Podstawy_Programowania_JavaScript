892b8c6eefd82bf942d54ffd97e2d6161477a798
Kapar-hub <kacper.barto727@gmail.com>
2020-09-09 13:59:05 +0200

    Zadanie 5 - done

---
+++ Zadanie5_WykonanieOperacjiArytmetycznych/js/main.js
@@ -0,0 +1,12 @@
+const x = 10, y = 20, z = 30;
+const result = x+y-z;
+console.log(`${x} + ${y} - ${z}`);
+console.log("Wynik obliczeń to: "+result);
+
+const result2 = x*y-z;
+console.log(`${x} * ${y} - ${z}`);
+console.log("Wynik obliczeń to: "+result2);
+
+const result3 = x*y/z;
+console.log(`${x} * ${y} / ${z}`);
+console.log("Wynik obliczeń to: "+result3);
