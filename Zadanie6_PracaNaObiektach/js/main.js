7c6a2719febb6eee29db8044dd2d94e8106f60fe
Kapar-hub <kacper.barto727@gmail.com>
2020-09-09 14:36:51 +0200

    Zadanie 6 - done

---
+++ Zadanie6_PracaNaObiektach/js/main.js
@@ -0,0 +1,8 @@
+const car = {
+    brand: "Peugeot",
+    model: "2008",
+    yearOfProduction: 2017,
+    color: 'szary'
+};
+
+console.log(`Mój samochód to ${car.color} ${car.brand} ${car.model} z ${car.yearOfProduction} roku.`);
